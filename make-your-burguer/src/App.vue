65677ff1484e8e0f13594a3d0e9e83d63dfd1f69
Israel Batista <israel.fbB@gmail.com>
2024-09-14 15:47:24 -0300

    feat: Aula 5

--- make-your-burguer/src/App.vue
+++ make-your-burguer/src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PrimeiroComponentVue />
+  <!-- o component pode ser adicionado igual a linha debaixo tbm -->
+  <!-- <primeiro-component-vue></primeiro-component-vue> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import PrimeiroComponentVue from './components/PrimeiroComponent.vue'
+  export default {
+    name: 'app',
+    components: {
+      PrimeiroComponentVue
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ make-your-burguer/src/components/PrimeiroComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Olá vue!</h1>
+</template>
+
+<script>
+export default {
+  name: "Primeiro Component"
+}
+</script>
+
+<style>
+
+</style>
